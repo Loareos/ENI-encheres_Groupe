0392fba48d1882373f1698475b3cd7ed6e49bd67
«LCB» <loic.cecyl@gmail.com>
2021-12-07 11:32:52 +0100

    class BO caté

---
+++ src/main/java/fr/eni/projet/BO/Categorie.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package fr.eni.projet.BO;
+
+/**
+ * @author junisaru69
+ *
+ */
+public class Categorie {
+	
+	private int  no_categorie; 
+	private String libelle; 
+
+////////////////////////////////////////////////////////////////
+//												Construct
+//______________________________________________________________
+	
+	/**
+	 * 
+	 */
+	public Categorie( String libelle) {
+		super();
+		this.libelle = libelle;
+	}
+	
+/**
+	 * @param no_categorie
+	 * @param libelle
+	 */
+	public Categorie(int no_categorie, String libelle) {
+		super();
+		this.no_categorie = no_categorie;
+		this.libelle = libelle;
+	}
+
+////////////////////////////////////////////////////////////////
+//												Get/Set
+//______________________________________________________________
+	/**
+	 * @return the no_categorie
+	 */
+	public int getNo_categorie() {
+		return no_categorie;
+	}
+	
+	/**
+	 * @param no_categorie the no_categorie to set
+	 */
+	public void setNo_categorie(int no_categorie) {
+		this.no_categorie = no_categorie;
+	}
+	
+	/**
+	 * @return the libelle
+	 */
+	public String getLibelle() {
+		return libelle;
+	}
+	
+	/**
+	 * @param libelle the libelle to set
+	 */
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+}
