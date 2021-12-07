34c7a4b3526fb80b08e4efed7bc88b8ebecab950
Goudot <etienne.goudot@hotmail.com>
2021-12-07 16:43:53 +0100

    Corrections sur le JdbcImpl + update fichier .gitignore

--- src/main/java/fr/eni/projet/dal/UserDAOJdbcImpl.java
+++ src/main/java/fr/eni/projet/dal/UserDAOJdbcImpl.java
@@ -14,8 +14,6 @@ import fr.eni.projet.BO.Utilisateur;
  *
  */
 
-
-
 public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 
 	String sqlInsert = "INSERT INTO UTILISATEUR (pseudo, nom, prenom, email, telephone, rue, code-postal, ville, mot_de_passe, credit, administrateur) VALUES (?,?,?,?,?,?,?,?,?,?,b?)";
@@ -36,7 +34,7 @@ public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 			stmt.setString(i++, utilisateur.getRue());
 			stmt.setString(i++, utilisateur.getCodePostal()); 
 			stmt.setString(i++, utilisateur.getVille());
-			stmt.setString(i++, utilisateur.getMotDePasse);
+			stmt.setString(i++, utilisateur.getMotDePasse());
 			stmt.setInt(i++, utilisateur.getCredit());
 			if(utilisateur.getAdministrateur())
 				stmt.setString(i++, "1");
@@ -48,7 +46,7 @@ public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 				utilisateur.setNoUtilisateur(rs.getInt(1));
 			}
 		}catch(SQLException e) {
-			return new DALException();
+			throw new DALException();
 		}
 	}
 
@@ -65,7 +63,7 @@ public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 			stmt.setString(i++, utilisateur.getRue());
 			stmt.setString(i++, utilisateur.getCodePostal()); 
 			stmt.setString(i++, utilisateur.getVille());
-			stmt.setString(i++, utilisateur.getMotDePasse);
+			stmt.setString(i++, utilisateur.getMotDePasse());
 			stmt.setInt(i++, utilisateur.getCredit());
 			if(utilisateur.getAdministrateur())
 				stmt.setString(i++, "1");
@@ -74,7 +72,7 @@ public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 			stmt.setInt(i++, utilisateur.getNoUtilisateur());
 			stmt.execute();
 		}catch(SQLException e) {
-			return new DALException();
+			throw new DALException();
 		}
 	}
 
@@ -86,6 +84,7 @@ public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 			try(ResultSet rs = stmt.executeQuery();){
 				rs.next();
 				Utilisateur u = new Utilisateur(idUtilisateur,  // ADAPTER L'ORDRE AVEC LE CONSTRUCTEUR
+							rs.getInt(10),	// CREDIT
 							rs.getString(1), // PSEUDO
 							rs.getString(2), // NOM
 							rs.getString(3), // PRENOM
@@ -95,12 +94,11 @@ public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 							rs.getString(7), // CODE POSTAL
 							rs.getString(8), // VILLE
 							rs.getString(9), // MOT DE PASSE
-							rs.getInt(10),	// CREDIT
-							rs.getString(11).equals(b"0") ? false : true); // ADMINISTRATEUR ===> PAS CERTAIN DE CE QUE JE VIENS DE FAIRE
+							rs.getString(11).equals("0") ? false : true); // ADMINISTRATEUR ===> PAS CERTAIN DE CE QUE JE VIENS DE FAIRE
 				return u;
 			}
 		}catch(SQLException e) {
-			return new DALException();
+			throw new DALException();
 		}
 	}
 
@@ -111,7 +109,7 @@ public class UserDAOJdbcImpl implements DAO<Utilisateur> {
 			stmt.setInt(1, idUtilisateur);
 			stmt.execute();
 		}catch(SQLException e) {
-			return new DALException();
+			throw new DALException();
 		}
 	}
 }
