a9c34721218c1eaf23b403f34abb37cf6e3dc30e
Goudot <etienne.goudot@hotmail.com>
2021-12-08 17:13:43 +0100

    Refonte du BO Utilisateur

--- src/main/java/fr/eni/projet/dal/DAO.java
+++ src/main/java/fr/eni/projet/dal/DAO.java
@@ -4,7 +4,7 @@ package fr.eni.projet.dal;
  *
  */
 
-
+// POURRA ETRE UTILISER AVEC LES DIFFERENTES INFOS A ENREGISTRER
 public interface DAO<T> {
 	public void insert(T obj) throws DALException;
 	public void update(T obj) throws DALException;
