0d536e086f38cc7370c5993315e4cba7420a15f0
RobinFerre <zazue@LAPTOP-BDK0CQVE>
2021-12-13 11:57:01 +0100

    Suppression et modification profil

--- src/main/java/fr/eni/projet/projetServlet/ModifProfilServlet.java
+++ src/main/java/fr/eni/projet/projetServlet/ModifProfilServlet.java
@@ -1,6 +1,7 @@
 package fr.eni.projet.projetServlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.projet.BusinessException;
+import fr.eni.projet.BLL.UtilisateurManager;
+import fr.eni.projet.BO.Utilisateur;
+import fr.eni.projet.messages.LecteurMessage;
 
 /**
  * Servlet implementation class ModifProfilServlet
@@ -32,16 +39,69 @@ public class ModifProfilServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String pseudo = request.getParameter("pseudo");
-		String Nom = request.getParameter("Nom");
-		String Prenom = request.getParameter("Prenom");
-		String Email = request.getParameter("Email");
-		String Telephone = request.getParameter("Telephone");
-		String Rue = request.getParameter("Rue");
-		String CodePostal = request.getParameter("CodePostal");
-		String Ville = request.getParameter("Ville");
-		String password = request.getParameter("password");
-		String passwordConfirm = request.getParameter("passwordConfirm");
-	}
+		String pseudoI = request.getParameter("PseudoI").trim();
+		String nomI = request.getParameter("NomI").trim();
+		String prenomI = request.getParameter("PrenomI").trim();
+		String emailI = request.getParameter("emailI").trim();
+		String rueI = request.getParameter("RueI").trim();
+		String telI = request.getParameter("telI").trim();
+		String codePostalI = request.getParameter("codePostalI").trim();
+		String villeI = request.getParameter("VilleI").trim();
+		String passwordI = request.getParameter("passwordI"); // Refuser les espaces dans le mot de passe
+		String passwordConfirmI = request.getParameter("passwordConfirmI");
 
+		// Modification Profil
+		try {
+			UtilisateurManager um = UtilisateurManager.getInstance();
+			Utilisateur user = um.modifProfil(null, pseudoI, nomI, prenomI, emailI, rueI, telI, codePostalI, villeI,
+					passwordI, passwordConfirmI);
+
+			// On valide comme pour se connecter
+			HttpSession sessionUser = request.getSession();
+			sessionUser.setAttribute("utilisateur", user);
+
+			RequestDispatcher rd = request.getRequestDispatcher("Profil.jsp");
+			rd.forward(request, response);
+
+		} catch (BusinessException e) {
+			StringBuffer sb = new StringBuffer();
+			sb.append("Une ou plusieurs erreurs se sont produites :");
+			for (int i : e.getListeCodesErreur())
+				sb.append("\n").append("CODE ").append(i).append(" - ").append(LecteurMessage.getMessageErreur(i));
+
+			System.err.println(sb.toString());
+
+			PrintWriter out = response.getWriter();
+			request.getRequestDispatcher("WEB-INF/jsp/Connexion/CreationCompte.jsp").include(request, response);
+			out.print(sb.toString());
+		}
+
+		// Supprimer Profil
+		try {
+			UtilisateurManager um = UtilisateurManager.getInstance();
+			um.suppressionUser(null);
+
+			// Comme pour la déconnexion renvoie à l'accueil
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			request.getRequestDispatcher("index.jsp").include(request, response);
+			out.print("Votre compte est supprimé");
+
+		} catch (BusinessException e) {
+
+			StringBuffer sb = new StringBuffer();
+			sb.append("Une ou plusieurs erreurs se sont produites :");
+			for (int i : e.getListeCodesErreur())
+				sb.append("\n").append("CODE ").append(i).append(" - ").append(LecteurMessage.getMessageErreur(i));
+
+			System.err.println(sb.toString());
+
+			PrintWriter out = response.getWriter();
+			request.getRequestDispatcher("WEB-INF/jsp/Connexion/CreationCompte.jsp").include(request, response);
+			out.print(sb.toString());
+
+		}
+
+	}
 }
