ad2f905ef004f17a5c30986ffa219e9a5416ce02
cjumeline2021 <cjumeline2021@DEV-1-EEL-1133.ad.campus-eni.fr>
2021-12-13 19:06:07 +0100

    Affichage de l'index quand l'url ne contient rien

--- src/main/java/fr/eni/projet/projetServlet/AccueilServlet.java
+++ src/main/java/fr/eni/projet/projetServlet/AccueilServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AccueilServlet
  */
-@WebServlet("/AccueilServlet")
+@WebServlet("/")
 public class AccueilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
