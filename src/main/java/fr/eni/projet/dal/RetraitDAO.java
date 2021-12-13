5b81ba9b66b68d1320696f1c3db9795a57b0776b
«LCB» <loic.cecyl@gmail.com>
2021-12-13 18:44:46 +0100

    rajout de modified by dans le commentaire des classe BO BLL, suppression des documents avec @author junisaru(LCB) quand ce n'était pas le cas. + modif fait sur la partie DAL que j'ai réalisé

--- src/main/java/fr/eni/projet/dal/RetraitDAO.java
+++ src/main/java/fr/eni/projet/dal/RetraitDAO.java
@@ -1,8 +1,10 @@
 package fr.eni.projet.dal;
 
+import fr.eni.projet.BusinessException;
 import fr.eni.projet.BO.Retrait;
 
 public interface RetraitDAO extends DAO<Retrait> {
 	
-	
+	public boolean verifRetrait(int noArticle) throws BusinessException;
+
 }
--- src/main/java/fr/eni/projet/dal/jdbc/RetraitDAOJdbcImpl.java
+++ src/main/java/fr/eni/projet/dal/jdbc/RetraitDAOJdbcImpl.java
@@ -1,38 +1,176 @@
 package fr.eni.projet.dal.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import fr.eni.projet.BusinessException;
 import fr.eni.projet.BO.Retrait;
 import fr.eni.projet.dal.CodesResultatDAL;
+import fr.eni.projet.dal.ConnectionProvider;
 import fr.eni.projet.dal.RetraitDAO;
 
 public class RetraitDAOJdbcImpl implements RetraitDAO {
 
+	String sqlInsert = "INSERT INTO RETRAITS (rue,code_postal,ville) "
+						+ "VALUES (?,?,?)";
+	
+	String sqlUpdate = "UPDATE RETRAITS SET rue = ?, code_postal = ?, ville = ? "
+						+ "WHERE no_article = ?";
+	
+	String sqlSelectById = "SELECT no_article,rue,code_postal,ville "
+						+ "FROM RETRAITS WHERE no_article = ?";
+	
+	String sqlDelete = "DELETE FROM RETRAITS WHERE no_article = ?";
+	
+	String sqlRetraitExiste = "SELECT COUNT(*) FROM RETRAITS WHERE no_article = ?";
+	
+
+	//=======================================================================//
+	//
+	//								INSERT
+	//
+	//=======================================================================//
+
 	@Override
 	public void insert(Retrait ret) throws BusinessException {
-		// TODO Auto-generated method stub
-		
+
+		checkNull(ret);
+
+		try (Connection con = ConnectionProvider.getConnection();
+				PreparedStatement stmt = con.prepareStatement(sqlInsert);){
+
+			int i = 1;
+			stmt.setString(i++, ret.getRue());
+			stmt.setString(i++, ret.getCode_postal());
+			stmt.setString(i++, ret.getVille());
+			stmt.execute();
+
+			try(ResultSet rs = stmt.getGeneratedKeys()){
+				rs.next();
+				ret.setNoArticle(rs.getInt(1));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.INSERT_OBJET_ECHEC);
+			throw businessException;
+		}
 	}
+
+	//=======================================================================//
+	//
+	//									UPDATE
+	//
+	//=======================================================================//
 
 	@Override
 	public void update(Retrait ret) throws BusinessException {
-		// TODO Auto-generated method stub
-		
+
+		checkNull(ret);
+
+		try(Connection cnx = ConnectionProvider.getConnection();
+				PreparedStatement stmt = cnx.prepareStatement(sqlUpdate);){
+
+			int i = 1;
+			stmt.setString(i++, ret.getRue());
+			stmt.setString(i++, ret.getCode_postal());
+			stmt.setString(i++, ret.getVille());
+			stmt.setInt(i++, ret.getNoArticle());
+			stmt.execute();
+
+		}catch(Exception e) {
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.UPDATE_OBJET_ECHEC);
+			throw businessException;
+		}
 	}
+
+	//=======================================================================//
+	//
+	//									SELECT BY ID
+	//
+	//=======================================================================//
 
 	@Override
 	public Retrait selectById(int idRet) throws BusinessException {
-		// TODO Auto-generated method stub
-		return null;
+
+		try(Connection cnx = ConnectionProvider.getConnection();
+				PreparedStatement stmt = cnx.prepareStatement(sqlSelectById);){
+
+			stmt.setInt(1, idRet);
+
+			try (ResultSet rs = stmt.executeQuery()){
+
+				Retrait retrait = new Retrait(idRet,
+						rs.getString("rue"),
+						rs.getString("code_postal"),
+						rs.getString("ville")
+						);
+				return retrait;
+			} 
+		}catch(Exception e) {
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.SELECT_OBJET_ECHEC);
+			throw businessException;
+		}
 	}
+
+	//=======================================================================//
+	//
+	//									DELETE
+	//
+	//=======================================================================//
 
 	@Override
 	public void delete(int idRet) throws BusinessException {
-		// TODO Auto-generated method stub
-		
+
+		try(Connection cnx = ConnectionProvider.getConnection();
+				PreparedStatement stmt = cnx.prepareStatement(sqlDelete);){
+
+			stmt.setInt(1, idRet);
+			stmt.execute();
+
+		}catch(Exception e) {
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.DELETE_OBJET_ECHEC);
+			throw businessException;
+		}
 	}
 	
-	public void checkNull(Retrait ret) throws BusinessException {
-		if(ret == null) {
+	//===========================verifRetrait=============================//
+	// renvoie true si il existe
+	
+	@Override
+	public boolean verifRetrait(int noArticle) throws BusinessException {
+		
+		try(Connection cnx = ConnectionProvider.getConnection();
+				PreparedStatement stmt = cnx.prepareStatement(sqlRetraitExiste);){
+			stmt.setInt(1, noArticle);
+			try(ResultSet rs = stmt.executeQuery();){
+				rs.next();
+				if(rs.getInt(1) > 0)
+					return true;
+				else
+					return false;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.IS_EXISTING_OBJET_ECHEC);
+			throw businessException;
+		}
+	}
+
+	//									
+	//==================================checkNull=============================//
+
+	public void checkNull(Retrait retrait) throws BusinessException {
+		if(retrait == null) {
 			BusinessException businessException = new BusinessException();
 			businessException.ajouterErreur(CodesResultatDAL.OBJET_NULL);
 			throw businessException;
