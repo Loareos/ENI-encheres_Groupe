0f0b46cf9ad262406edada675e2167ebfd632e5a
RobinFerre <zazue@LAPTOP-BDK0CQVE>
2021-12-13 17:08:16 +0100

    Création Article manager en cours

---
+++ src/main/java/fr/eni/projet/BLL/ArticleManager.java
@@ -0,0 +1,52 @@
+package fr.eni.projet.BLL;
+
+import fr.eni.projet.BusinessException;
+import fr.eni.projet.BO.Categorie;
+import fr.eni.projet.dal.ArticleDAO;
+import fr.eni.projet.dal.DAOFactory;
+
+public class ArticleManager {
+	private ArticleDAO articleDao;
+	private static ArticleManager instance;
+
+	public static ArticleManager getInstance() throws BusinessException {
+		if (instance == null) {
+			try {
+				instance = new ArticleManager();
+			} catch (Exception e) {
+				e.printStackTrace();
+				BusinessException businessException = new BusinessException();
+				businessException.ajouterErreur(CodesResultatBLL.GET_INSTANCE);
+				throw businessException;
+			}
+		}
+		return instance;
+	}
+
+	private ArticleManager() throws BusinessException {
+		articleDao = DAOFactory.getArticleDAO();
+	}
+
+	// ========== CREATION ARTICLE=========
+
+	// public Article ajouterArticleStandard(String nom, String description, String
+	// prix) {
+	// BusinessException exception = new BusinessException();
+
+	// Article art = new Article(nom, description, prix);
+
+	// ============CATEGORIE=============
+
+	public Categorie ajouterCategorie(String informatique, String utilitaire, String manutention) {
+		BusinessException exception = new BusinessException();
+		return null;
+
+	}
+
+//================MODIF CATEGORIE===============
+	public Categorie modifCategorie(String nom, String description, String prix) {
+		BusinessException exception = new BusinessException();
+		return null;
+
+	}
+}
