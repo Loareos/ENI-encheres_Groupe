17e60721504bc2a31ad69f28403b12f8d6766435
cjumeline2021 <cjumeline2021@DEV-1-EEL-1133.ad.campus-eni.fr>
2021-12-17 11:47:02 +0100

    erreur corrigée

--- src/main/java/fr/eni/projet/projetServlet/AccueilServlet.java
+++ src/main/java/fr/eni/projet/projetServlet/AccueilServlet.java
@@ -24,61 +24,46 @@ import fr.eni.projet.messages.LecteurMessage;
  */
 /**
  * Servlet implementation class AccueilServlet
- */
-@WebServlet("/")
-public class AccueilServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+ */@WebServlet("/")
+ public class AccueilServlet extends HttpServlet {
+		private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		try {
-			ArticleVenduManager avm = ArticleVenduManager.getInstance();
-			List<ArticleVendu> lstArtEnCours = avm.getArticlesEnVente();
-			request.setAttribute("listeArticles", lstArtEnCours);
-
-			int i = 0;
-			List<ArticleVendu> articles = new ArrayList<ArticleVendu>();
-			List<Integer> miseAPrix = new ArrayList<Integer>();
-			List<Integer> MeilleureOffre = new ArrayList<Integer>();
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+		 *      response)
+		 */
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
 			
-			while (i < lstArtEnCours.size()) {
-				articles.add(lstArtEnCours.get(i));
+			try {
+				ArticleVenduManager avm = ArticleVenduManager.getInstance();
+				List<ArticleVendu> lstArtEnCours = avm.getArticlesEnVente();
+				request.setAttribute("listeArticles", lstArtEnCours);
 				
-				ArticleVendu article = articles.get(i);
-				
-				i += 1;
+				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("article", null);
+				request.setAttribute("acheteur", null);
+				rd.forward(request, response);			
+
+			} catch (BusinessException e) {
+				StringBuffer sb = new StringBuffer();
+				sb.append("Une ou plusieurs erreurs se sont produites :");
+				for(int i : e.getListeCodesErreur())
+					sb.append("\n").append("CODE ").append(i).append(" - ").append(LecteurMessage.getMessageErreur(i));
+
+				PrintWriter out = response.getWriter();
+				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request, response);
+				out.print(sb.toString());
 			}
+		}
 
-			
-			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-			request.setAttribute("article", null);
-			request.setAttribute("acheteur", null);
-			rd.forward(request, response);			
-
-		} catch (BusinessException e) {
-			StringBuffer sb = new StringBuffer();
-			sb.append("Une ou plusieurs erreurs se sont produites :");
-			for(int i : e.getListeCodesErreur())
-				sb.append("\n").append("CODE ").append(i).append(" - ").append(LecteurMessage.getMessageErreur(i));
-
-			PrintWriter out = response.getWriter();
-			RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-			rd.forward(request, response);
-			out.print(sb.toString());
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+		 *      response)
+		 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			doGet(request, response);
 		}
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
-}
